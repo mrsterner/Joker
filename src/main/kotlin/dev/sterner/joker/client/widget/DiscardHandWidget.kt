e6618b966699f9a7ccfcd5c77a73bf7bcb45e92c
mrsterner <simon@sterner.dev>
2024-06-11 16:17:11 +0200

    now it is cool

--- src/main/kotlin/dev/sterner/joker/client/widget/DiscardHandWidget.kt
+++ src/main/kotlin/dev/sterner/joker/client/widget/DiscardHandWidget.kt
@@ -3,8 +3,10 @@ package dev.sterner.joker.client.widget
 import com.mojang.blaze3d.systems.RenderSystem
 import dev.sterner.joker.JokerMod
 import dev.sterner.joker.client.GameScreen
+import net.minecraft.client.Minecraft
 import net.minecraft.client.gui.GuiGraphics
 import net.minecraft.client.gui.components.WidgetSprites
+import net.minecraft.network.chat.Component
 
 class DiscardHandWidget(screen: GameScreen, x: Int, y: Int, width: Int, height: Int) : AbstractGameWidget(
     screen, x, y, width, height
@@ -20,6 +22,10 @@ class DiscardHandWidget(screen: GameScreen, x: Int, y: Int, width: Int, height: 
     override fun renderWidget(guiGraphics: GuiGraphics, mouseX: Int, mouseY: Int, partialTick: Float) {
         RenderSystem.enableBlend()
         guiGraphics.blitSprite(SPRITES[false, this.isHoveredOrFocused], this.x, this.y, this.width, this.height)
+        guiGraphics.pose().pushPose()
+        guiGraphics.pose().scale(0.66f, 0.66f, 1.0f)
+        guiGraphics.drawCenteredString(Minecraft.getInstance().font, Component.literal("Discard"), (this.x * 1.5).toInt() + width - 4, (this.y * 1.5).toInt() + height - 6, 0xFFFFFF)
+        guiGraphics.pose().popPose()
         RenderSystem.disableBlend()
     }
 
